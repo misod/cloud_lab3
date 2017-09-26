3fad8833029a154f12ae0328a610e90f37038f31
Mikael söderberg <mikael.misod@gmail.com>
2017-09-26 12:22:21 +0200

    tjoo

--- celery.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-
-app = Celery('test_celery',
-             broker='amqp://jag:jag123@localhost/jag_vhost',
-             backend='rpc://',
-             include=['test_celery.tasks'])
