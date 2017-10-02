88cbb1d3608b390e9e272bacbbbf278b0c3c5a30
Mikael söderberg <mikael.misod@gmail.com>
2017-10-02 20:09:37 +0200

    test

--- celery_parser/celery.py
+++ celery_parser/celery.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('celery_parser',
-             broker='amqp://micke:micke123@localhost/micke_vhost',
+             broker='amqp://micke:jag123@localhost/micke_vhost',
              backend='rpc://',
              include=['celery_parser.parsning'])
